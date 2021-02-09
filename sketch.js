4f365e3fc35a079f7ebcf76878ff432246778f20
TejaR2008 <74098883+TejaR2008@users.noreply.github.com>
2021-02-09 18:49:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database, position;
+var form, game, player;
+var gameState = 0;
+var playerCount;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+
+    game = new Game()
+    game.getState()
+    game.start()
+
+}
+
+function draw(){
+    background("white");
+
+
+    drawSprites();
+    
+    }
+
+
+
+
+
